989c08e1a27ec76e91aebb6f80c9badebad295bc
Casnail <yuya090202@gmail.com>
2018-01-31 21:39:20 +0900

    add "get"

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HomePage } from './home';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+
+@NgModule({
+    declarations: [
+        HomePage,
+    ],
+    imports: [
+        IonicPageModule.forChild(HomePage),
+    ],
+})
+export class HomePageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { IonicPage, NavController } from 'ionic-angular';
+
+@IonicPage()
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts:{
+    ID: number,
+    title: string,
+    content: string,
+    date: string,
+  }[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+  public http: HttpClient
+) {}
 
-  }
-
+ionViewDidLoad(){
+  this.http
+  .get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts')
+  .subscribe(data => {
+    this.posts = data['posts'];
+  })
+}
 }
