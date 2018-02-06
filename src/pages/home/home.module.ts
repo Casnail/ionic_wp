0d5eb0de19e159794179c168ec0a69499f7616c8
Casnail <yuya090202@gmail.com>
2018-02-07 00:36:47 +0900

    error fix
    ビルドには影響のないエラーの修正

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
-import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+//import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
 @NgModule({
     declarations: [
